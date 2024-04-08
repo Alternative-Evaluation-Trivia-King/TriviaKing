e8fe7fc2864c6e25b1e9f857b24101920373f682
Yehonatan28 <kidushim@post.bgu.ac.il>
2024-04-08 22:17:32 +0300

    08.4.24

--- Client.py
+++ Client.py
@@ -1,10 +1,10 @@
 import random
 import socket
 import time
-
 import keyboard
 
-# Server configuration
+'''----------------------------------------- Global variable --------------------------------------------------'''
+
 BROADCAST_PORT = 13117
 names = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Hannah", "Isaac", "Julia", "Kevin", "Linda",
          "Michael", "Nancy", "Olivia"]
@@ -19,35 +19,35 @@ def client():
     client_UDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client_UDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    # Bind the socket to the server address and port
+    # Bind the socket to the Broadcast
     client_UDP.bind(('0.0.0.0', BROADCAST_PORT))
 
     print("Client started, listening for offer requests...")
 
     while True:
         try:
+            # Receives data from the server over the UDP socket and stores the received data
             data, server_address = client_UDP.recvfrom(1024)
-            magic_cookie = int.from_bytes(data[:4], byteorder='big')
-            message_type = int.from_bytes(data[4:5], byteorder='big')
-            server_name = data[5:37].decode('utf-8').strip('\x00')  # Assuming the server name is Unicode encoded
-            server_port = int.from_bytes(data[37:39], byteorder='big')
 
-            if magic_cookie != 0xabcddcba or message_type != 0x2:
-                print("The received package does not meet certain criteria")
+            # Extract packet from server
+            server_name, server_port, isValid = ExtractPacketFromServer(data)
+            if not isValid:
                 continue
 
-            print("Received offer from server " + server_name + " at address " + server_address[
-                0] + ", attempting to connect...")
+            print(f"Received offer from server {server_name} at address {server_address[0]}, attempting to connect...")
 
             # Connect to the server over TCP
             client_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Establishes a connection for client sockets to a remote server.
             client_TCP.connect((server_address[0], server_port))
             print("Connected to server over TCP.")
 
             # Choose a name randomly
             player_name = random.choice(names)
+            # Send the name to the server
             client_TCP.sendall(player_name.encode() + b'\n')
 
+            # Start play
             clientPlay(client_TCP)
 
         except KeyboardInterrupt:
@@ -56,6 +56,20 @@ def client():
 
     # Close the socket
     client_UDP.close()
+
+
+def ExtractPacketFromServer(data):
+    isValid = True
+    magic_cookie = int.from_bytes(data[:4], byteorder='big')
+    message_type = int.from_bytes(data[4:5], byteorder='big')
+    server_name = data[5:37].decode('utf-8').strip('\x00')
+    server_port = int.from_bytes(data[37:39], byteorder='big')
+
+    if magic_cookie != 0xabcddcba or message_type != 0x2:
+        print("The received package does not meet certain criteria")
+        isValid = False
+
+    return server_name, server_port, isValid
 
 
 def clientPlay(client_TCP):
